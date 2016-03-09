1090b60a720521a8d8308d648d179cd2356625b4
alev <lokodenis73@gmail.com>
2016-03-09 15:03:15 +0300

    dealed with matching bug

--- detectors.cpp
+++ detectors.cpp
@@ -13,9 +13,9 @@ using namespace cv::xfeatures2d;
 int main() {
     Mat src = imread("/home/oracle/Project/Images/lena.jpg", CV_LOAD_IMAGE_UNCHANGED);
     Mat sec_src = imread("/home/oracle/Project/Images/exam.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-    Mat third_src = imread("/home/oracle/Project/Images/4th2.jpg", CV_LOAD_IMAGE_UNCHANGED);
-    Ptr<Feature2D> f2d = SURF::create(800);
-    Ptr<Feature2D> f2d_sift = SIFT::create(0, 3, 0.2, 10, 1.2); //  trying cycles
+    Mat third_src = imread("/home/oracle/Project/Images/image.jpg", CV_LOAD_IMAGE_UNCHANGED);
+    Ptr<Feature2D> f2d = SURF::create(1000);
+    Ptr<Feature2D> f2d_sift = SIFT::create(0, 3, 0.2 , 15, 1.2); //  trying cycles
     // Setup SimpleBlobDetector parameters.
     SimpleBlobDetector::Params params;
 
@@ -31,8 +31,9 @@ int main() {
     warpAffine(sec_src, sec_src_rotated, sec_rot_mat, sec_src.size());
 
     cv::Mat third_src_rotated;
+    int angle = 20;
     cv::Point2d third_src_center(third_src.cols * 0.5, third_src.rows * 0.5); // defining a center of the source picture
-    cv::Mat third_rot_mat = cv::getRotationMatrix2D(third_src_center, 0 , 1);  // creating a rotation matrix
+    cv::Mat third_rot_mat = cv::getRotationMatrix2D(third_src_center, angle , 1);  // creating a rotation matrix
     warpAffine(third_src, third_src_rotated, third_rot_mat, third_src.size());
 
     // Change thresholds
@@ -58,59 +59,58 @@ int main() {
 
     Ptr<SimpleBlobDetector> blobe_detector = SimpleBlobDetector::create(params);
 
-    //detecting and computing
-
+    // inverting colors
+//
 //    for (size_t i = 0; i != third_src_rotated.rows; ++i) {
 //        for (size_t j = 0; j != third_src_rotated.cols; ++ j) {
-//            third_src_rotated.at<cv::Vec3b>(i, j)[0] = static_cast<uchar> (255 - third_src_rotated.at<cv::Vec3b>(i, j)[0]);
-//            third_src_rotated.at<cv::Vec3b>(i, j)[1] = static_cast<uchar> (255 - third_src_rotated.at<cv::Vec3b>(i, j)[1]);
-//            third_src_rotated.at<cv::Vec3b>(i, j)[2] = static_cast<uchar> (255 - third_src_rotated.at<cv::Vec3b>(i, j)[2]);
+//            third_src_rotated.at<cv::Vec3b>(i, j)[0] = static_cast<uchar> (third_src_rotated.at<cv::Vec3b>(i, j)[0] + 1);
+//            third_src_rotated.at<cv::Vec3b>(i, j)[1] = static_cast<uchar> (third_src_rotated.at<cv::Vec3b>(i, j)[1] + 1);
+//            third_src_rotated.at<cv::Vec3b>(i, j)[2] = static_cast<uchar>  (third_src_rotated.at<cv::Vec3b>(i, j)[2] + 1);
 //        }
 //    }
+
+    //resize in case image too large to fill the screen
+    Mat sized_src_one;
+    Mat sized_src_two;
+    Mat sized_src_blobe_one;
+    Mat sized_src_blobe_two;
+    Mat sized_src_sift_one;
+    Mat sized_src_sift_two;
+
+    resize(src, sized_src_one, Size(600, 600), 0, 0, INTER_LINEAR);
+    resize(src_rotated, sized_src_two, Size(600, 600), 0, 0, INTER_LINEAR);
+    resize(sec_src, sized_src_blobe_one, Size(600, 600), 0, 0, INTER_LINEAR);
+    resize(sec_src_rotated, sized_src_blobe_two, Size(600, 600), 0, 0, INTER_LINEAR);
+    resize(third_src, sized_src_sift_one, Size(800, 600), 0, 0, INTER_LINEAR);
+    resize(third_src_rotated, sized_src_sift_two, Size(800, 600), 0, 0, INTER_LINEAR);
+
+    // descriptors and detectors
 
     Mat descriptor_one, descriptor_two, descriptor_sift_one, descriptor_sift_two;
     std::vector<KeyPoint> points_one, points_two, blobe_points_one, blobe_points_two;
     std::vector<KeyPoint> points_sift_one, points_sift_two;
-    f2d -> detectAndCompute(src, Mat(), points_one, descriptor_one); //surf
-    f2d -> detectAndCompute(src_rotated, Mat(), points_two, descriptor_two);
-    f2d_sift -> detectAndCompute(third_src, Mat(), points_sift_one, descriptor_sift_one);
-    f2d_sift -> detectAndCompute(third_src_rotated, Mat(), points_sift_two, descriptor_sift_two);
-    blobe_detector -> detect(sec_src, blobe_points_one);
-    blobe_detector -> detect(sec_src_rotated, blobe_points_two);
+    f2d -> detectAndCompute(sized_src_one, Mat(), points_one, descriptor_one); //surf
+    f2d -> detectAndCompute(sized_src_two, Mat(), points_two, descriptor_two);
+    f2d_sift -> detectAndCompute (sized_src_sift_one, Mat(), points_sift_one, descriptor_sift_one);
+    f2d_sift -> detectAndCompute(sized_src_sift_two, Mat(), points_sift_two, descriptor_sift_two);
+    blobe_detector -> detect(sized_src_blobe_one, blobe_points_one);
+    blobe_detector -> detect(sized_src_blobe_two, blobe_points_two);
+
+    //drawing keypoints
 
     Mat keys_one, keys_two, blobe_keys_one, blobe_keys_two, sift_keys_one, sift_keys_two;
 
-    drawKeypoints(src, points_one, keys_one, Scalar(0, 0, 255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-    drawKeypoints(src_rotated, points_two, keys_two, Scalar(0, 0, 255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-    drawKeypoints(sec_src, blobe_points_one, blobe_keys_one, Scalar(0,0,255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-    drawKeypoints(sec_src_rotated, blobe_points_two, blobe_keys_two, Scalar(0,0,255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-    drawKeypoints(third_src, points_sift_one, sift_keys_one, Scalar(0,0,255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-    drawKeypoints(third_src_rotated, points_sift_two, sift_keys_two, Scalar(0,0,255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-
-
-    // image too large to fill the screen
-    Mat sized_keys_one;
-    Mat sized_keys_two;
-    Mat sized_keys_blobe_one;
-    Mat sized_keys_blobe_two;
-    Mat sized_keys_sift_one;
-    Mat sized_keys_sift_two;
-
-//    sized_keys_sift_one = sift_keys_one.clone();
-//    sized_keys_sift_two = sift_keys_two.clone();
-
-    resize(keys_one, sized_keys_one, Size(600, 600), 0, 0, INTER_LINEAR);
-    resize(keys_two, sized_keys_two, Size(600, 600), 0, 0, INTER_LINEAR);
-    resize(blobe_keys_one, sized_keys_blobe_one, Size(600, 600), 0, 0, INTER_LINEAR);
-    resize(blobe_keys_two, sized_keys_blobe_two, Size(600, 600), 0, 0, INTER_LINEAR);
-    resize(sift_keys_one, sized_keys_sift_one, Size(600, 800), 0, 0, INTER_LINEAR);
-    resize(sift_keys_two, sized_keys_sift_two, Size(600, 800), 0, 0, INTER_LINEAR);
-
+    drawKeypoints(sized_src_one, points_one, keys_one, Scalar(0, 0, 255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    drawKeypoints(sized_src_two, points_two, keys_two, Scalar(0, 0, 255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    drawKeypoints(sized_src_blobe_one, blobe_points_one, blobe_keys_one, Scalar(0,0,255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    drawKeypoints(sized_src_blobe_two, blobe_points_two, blobe_keys_two, Scalar(0,0,255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    drawKeypoints(sized_src_sift_one, points_sift_one, sift_keys_one, Scalar(0,0,255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    drawKeypoints(sized_src_sift_two, points_sift_two, sift_keys_two, Scalar(0,0,255), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
     namedWindow("SURF", CV_WINDOW_AUTOSIZE);
     namedWindow("SURF2", CV_WINDOW_AUTOSIZE);
-    imshow ("SURF", sized_keys_one);
-    imshow ("SURF2", sized_keys_two);
+    imshow ("SURF", keys_one);
+    imshow ("SURF2", keys_two);
     waitKey(0);
 
 //    destroyWindow("SURF");
@@ -118,27 +118,28 @@ int main() {
 
     namedWindow("BLOBE", CV_WINDOW_AUTOSIZE);
     namedWindow("BLOBE2", CV_WINDOW_AUTOSIZE);
-    imshow ("BLOBE", sized_keys_blobe_one);
-    imshow ("BLOBE2", sized_keys_blobe_two);
+    imshow ("BLOBE", blobe_keys_one);
+    imshow ("BLOBE2", blobe_keys_two);
     waitKey(0);
     destroyWindow("BLOBE");
     destroyWindow("BLOBE2");
 
     namedWindow("SIFT", CV_WINDOW_AUTOSIZE);
     namedWindow("SIFT2", CV_WINDOW_AUTOSIZE);
-    imshow ("SIFT", sized_keys_sift_one);
-    imshow ("SIFT2", sized_keys_sift_two);
+    imshow ("SIFT", sift_keys_one);
+    imshow ("SIFT2", sift_keys_two);
     waitKey(0);
     destroyWindow("SIFT");
     destroyWindow("SIFT2");
 
-    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/surf1.jpg", sized_keys_one);
-    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/surf2.jpg", sized_keys_two);
-    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/blobe1.jpg", sized_keys_blobe_one);
-    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/blobe2.jpg", sized_keys_blobe_two);
-    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/sift1.jpg", sized_keys_sift_one);
-    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/sift1.jpg", sized_keys_sift_two);
+    //writing keypoints to files
 
+    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/surf1.jpg", keys_one);
+    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/surf2.jpg", keys_two);
+    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/blobe1.jpg", blobe_keys_one);
+    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/blobe2.jpg", blobe_keys_two);
+    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/sift1.jpg", sift_keys_one);
+    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/sift1.jpg", sift_keys_two);
 
     //matching
     BFMatcher matcher (NORM_L2, true);
@@ -148,21 +149,23 @@ int main() {
     matcher.match(descriptor_one, descriptor_two, surf_matches_vector);
     matcher_sift.match(descriptor_sift_one, descriptor_sift_two, sift_matches_vector);
 
-    //some results
+    //showing results of matching
     namedWindow("SUFR_matches", 1);
     Mat surf_matches;
-    drawMatches(sized_keys_one, points_one, sized_keys_two, points_two, surf_matches_vector, surf_matches);
+    drawMatches(keys_one, points_one, keys_two, points_two, surf_matches_vector, surf_matches);
     imshow ("SURF_matches", surf_matches);
 
     waitKey(0);
 
     namedWindow("SIFT_matches", 1);
     Mat sift_matches;
-    drawMatches(sized_keys_sift_one, points_sift_one, sized_keys_sift_two, points_sift_two, sift_matches_vector, sift_matches);
+    drawMatches(sift_keys_one, points_sift_one, sift_keys_two, points_sift_two, sift_matches_vector, sift_matches);
     imshow ("SIFT_matches", sift_matches);
 
     waitKey(0);
-
+    
+    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/surf_matches.jpg", surf_matches);
+    imwrite("/home/oracle/ClionProjects/comvision/Detected_dotes/siftf_matches.jpg", sift_matches);
 
     return 0;
 }
